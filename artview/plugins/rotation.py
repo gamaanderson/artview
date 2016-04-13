44d2145208bddeb420479b1b54c5f744b18c96c8
Gama Anderson <gamaanderson92@gmail.com>
2016-04-13 19:48:59 +0200

    add example file

--- artview/plugins/rotation.py
+++ artview/plugins/rotation.py
@@ -3,11 +3,20 @@ cmap_value.py
 """
 
 # Load the needed packages
+import csv
 import pyart
 import numpy as np
+import os
+#import sys
+#sys.path.append('artview')
 from .. import core
 
 class Rotation(core.Component):
+
+    csvdata = np.genfromtxt(os.path.dirname(__file__) + '/data_1.csv',
+                            dtype=float,
+                            delimiter=',')
+    datapt =0
 
     @classmethod
     def guiStart(self, parent=None):
@@ -16,12 +25,25 @@ class Rotation(core.Component):
         kwargs['parent'] = parent
         return self(**kwargs), independent
 
+#    def xyz(self):
+#        with open(os.path.dirname(__file__) + '/data_1.csv', 'rb') as csvfile:
+#            spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+#            x=0
+#            for row in spamreader:
+#               if(x!=0):
+#                   self.csvdata =row[0]
+#               else:
+#                   x=1
+
+        #print "------------------------"+self.csvdata
+#        print self.csvdata[0]
+
     def __init__(self, name="Rotation", parent=None):
         '''
         make test radar for animation
         '''
         super(Rotation, self).__init__(name=name, parent=parent)
-
+        #self.xyz()
 
         self.central_widget = core.QtGui.QWidget()
         self.setCentralWidget(self.central_widget)
@@ -32,10 +54,10 @@ class Rotation(core.Component):
         self.running = False
 
         radar = pyart.testing.make_target_radar()
-        radar.range['data'] = 100 * radar.range['data']
+        radar.range['data'] = 26.5 * radar.range['data']
         self.field = list(radar.fields.keys())[0]
         radar.fields[self.field]['data']=0*radar.fields[self.field]['data']
-        self.seed=0
+        self.seed=90
 
         self.Vradar = core.Variable(radar)
 
@@ -61,9 +83,19 @@ class Rotation(core.Component):
             self.timer.start(100)
 
     def loop(self):
-        data=(np.sin(np.arange(50)*0.1+self.seed/50.)+1)*25
+        #data=(np.sin(np.arange(50)*0.1+self.seed/50.)+1)*25
+#        self.datapt = self.datapt+1
+#        data=self.csvdata[self.datapt]
+#        print self.csvdata
+#        print data
+#        if(self.datapt>=360):
+#            self.datapt=0
+
         ray = np.mod(self.seed,360)
-        self.Vradar.value.fields[self.field]['data'][ray] = data
+        if ray == 359:
+            #example file has only 359 rays
+            ray = 358
+        self.Vradar.value.fields[self.field]['data'][ray] = self.csvdata[ray]
         self.seed=self.seed+1
         self.Vradar.update()
 
